6dea4c47a22d78f513fd361892f369f97fab1b08
Scott Serage <xttocsurge51@gmail.com>
2017-09-08 12:30:10 -0600

    Successfully created a simple React app that displayed my name

---
+++ app/src/components/Calculator.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import calculatorImg from './calculator.png'
+
+class Calculator extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      header: "Scott's Calculator"
+    }
+  }
+
+  render(){
+    return (
+      <div id="calculator-container">
+        <input id="header-input"/>
+        <h1 id="header"> { this.state.header }</h1>
+        <img className="remove-highlight" src={calculatorImg} alt="calculator" />
+        <div id="calculator-mask" className="remove-highlight">
+          <div className="output">
+            <span className="total"></span>
+          </div>
+
+          <div className="btn clear"></div>
+
+          <div className="btn zero"></div>
+          <div className="btn one"></div>
+          <div className="btn two"></div>
+          <div className="btn three"></div>
+          <div className="btn four"></div>
+          <div className="btn five"></div>
+          <div className="btn six"></div>
+          <div className="btn seven"></div>
+          <div className="btn eight"></div>
+          <div className="btn nine"></div>
+
+          <div className="btn equal"></div>
+          <div className="btn multiply"></div>
+          <div className="btn divide"></div>
+          <div className="btn subtract"></div>
+          <div className="btn add"></div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Calculator;
